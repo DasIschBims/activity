32f829cfd0d67bd85c711b3cc3af044be73d31ab
DasIschBims <you@example.com>
2021-12-21 17:10:40 +0100

    Update

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,23 +1,27 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed, Client } = require('discord.js')
+const os = require('os');
 
 module.exports = {
      data: new SlashCommandBuilder()
              .setName('stats')
              .setDescription("Sends stats about the bot."),
-  async execute(interaction) {
+  async execute(interaction, client) {
 
     const uptime = (interaction.client.uptime / 1000)
     const days = Math.floor(uptime / 86400)
-    var hours = Math.floor(uptime / 3600)
-    var minutes = Math.floor(uptime / 60 / 60)
+    var totalhours = Math.floor(days * 24)
+    var hours = Math.floor(uptime / 3600 - totalhours)
+    var totalminutes = Math.floor(hours * 60)
+    var minutes = Math.floor(uptime / 60 - totalminutes)
     var seconds = Math.floor(uptime % 60)
 
-    hours = (hours < 10) ? + hours : hours;
-    minutes = (minutes < 10) ? + minutes : minutes;
-    seconds = (seconds < 10) ? + seconds : seconds;
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0"  + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
 
     const totaluptime = `${days}d, ${hours}h, ${minutes}m, ${seconds}s`
+
          interaction.reply({embeds: [
            new MessageEmbed()
            .setTitle('Bot stats')
@@ -27,12 +31,23 @@ module.exports = {
            .addFields([
              {
                name: 'Ping',
-               value: "`" + `${Date.now() - interaction.createdTimestamp}` + "ms`"
+               value: "`" + `${Date.now() - interaction.createdTimestamp}` + "ms`",
+               inline: true
              },
              {
-               name: 'Uptime',
-               value: "`" + `${totaluptime}` + "`"
-             }
+              name: 'API Latency',
+              value: "`" + `${Math.round(interaction.client.ws.ping)}` + "ms`",
+              inline: true
+            },
+            {
+              name: 'OS',
+              value: "`" + `${os.type} ${os.arch}` + "`",
+              inline: true
+            },
+            {
+              name: 'Uptime',
+              value: "`" + `${totaluptime}` + "`"
+            }
            ])
          ]})
     }
